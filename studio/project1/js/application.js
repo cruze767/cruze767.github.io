494e2d5d0c7c1288c9d6ea54eae46aaebac52408
cruze767 <cruze767@newschool.edu>
2015-03-05 16:21:40 +0000

    message

--- studio/project1/js/application.js
+++ studio/project1/js/application.js
@@ -10,3 +10,33 @@ $('.close').click(function(event) {
     $('.nav').removeClass('show');
 });
 
+
+
+
+
+$('.navup').click(function(event) {
+    
+    $('.nav').removeClass('show');
+    
+});
+
+$('.menuicon1').click(function(event) {
+
+    
+    $('.nav').removeClass('show');
+    
+});
+
+$('.menuicon2').click(function(event) {
+    
+    $('.nav').removeClass('show');
+    
+});
+
+$('.menuicon3').click(function(event) {
+
+    
+    $('.nav').removeClass('show');
+
+});
+
