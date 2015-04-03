925e105cda844ee1084e5792a62c0b6108a4a25d
cruze767 <cruze767@newschool.edu>
2015-04-03 15:35:59 +0000

    update the helper function to show and hide helper text on hover

--- studio/project2/js/application.js
+++ studio/project2/js/application.js
@@ -1,6 +1,7 @@
-$('.wrapper_d').hover(function() {
-    alert('hi');
+$('.helper').hover(function() {
     var helper_text = $(this).data('helper');
     
     $('.helper').html(helper_text);
+}, function() {
+    $('.helper').html('');
 });
