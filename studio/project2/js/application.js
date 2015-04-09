fe00cd31bf54017d948626db3a5ee65472625994
cruze767 <cruze767@newschool.edu>
2015-04-09 18:33:56 +0000

    add all animations to divs

--- studio/project2/js/application.js
+++ studio/project2/js/application.js
@@ -5,3 +5,31 @@ $('.helper').hover(function() {
 }, function() {
     $('.helper-text').html('');
 });
+
+
+
+$(document).ready(function(){
+    animateDiv();
+    
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    $('.ex_wrapper').animate({ top: newq[0], left: newq[1] }, function(){
+      animateDiv();        
+    });
+    
+};
