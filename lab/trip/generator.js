4206dc3e88fdf34e4c7d9eae08ad82d2dd3849d2
cruze767 <cruze767@newschool.edu>
2015-05-14 11:03:50 +0000

    uoload gifs

--- lab/trip/generator.js
+++ lab/trip/generator.js
@@ -9,7 +9,14 @@ var gifs = ["http://media.giphy.com/media/l41m0D66I0lUQZIQw/giphy.gif",
 	"http://media.giphy.com/media/xTiTnDP8WpEsyitxTO/giphy.gif",
 	"http://media.giphy.com/media/3oEduKC1gvOmgY46Ag/giphy.gif",
 	"http://media.giphy.com/media/l41lNqEeM4aHMwK0E/giphy.gif",
-	"http://media.giphy.com/media/l41lOIaX2Ri0grAhG/giphy.gif"
+	"http://media.giphy.com/media/l41lOIaX2Ri0grAhG/giphy.gif",
+	"http://media.giphy.com/media/3o85xuFLynTZOcGQSI/giphy.gif",
+	"http://media.giphy.com/media/3oEdv4OG24o7cRehvq/giphy.gif",
+	"http://media.giphy.com/media/3oEdv6SgY5GZbygDbW/giphy.gif",
+	"http://media.giphy.com/media/l41lZht6GAwpRUdqg/giphy.gif",
+	"http://media.giphy.com/media/3oEdvbkwheRnc1Cg4E/giphy.gif",
+	"http://media.giphy.com/media/l41lIjeFPHXBVDGyA/giphy.gif",
+	"http://media.giphy.com/media/l41lYOpdcl0ew505O/giphy.gif"
 	];
 	
 
